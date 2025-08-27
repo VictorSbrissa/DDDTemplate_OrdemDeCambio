e652efbeede6efd0686f6aece39a0383ab28e878
victo <victo@SBRISSA>
2025-08-26 22:35:21 -0300

    Commit - testes unitarios

--- CambioDDD.ConsoleApp/Program.cs
+++ CambioDDD.ConsoleApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CambioDDD.Application.Services;
+using CambioDDD.Domain.Orders;
+using CambioDDD.Infrastructure.Repositories;
+
+var repo = new OrdemDeCambioRepositoryInMemory();
+var service = new OrdemDeCambioService(repo);
+
+var moedaOrigem = new Moeda("USD", "Dólar");
+var moedaDestino = new Moeda("BRL", "Real");
+
+
+for (int i = 1; i < 5; i++)
+{
+    service.Adicionar(i, moedaOrigem, moedaDestino);
+}
+
+foreach (var order in service.ObterTodas())
+{
+    Console.WriteLine($"Ordem: {order.OrderId} , valor: {order.ValorOperacao} , moeda origem: {order.MoedaOrigem.Nome} , moeda destino: {order.MoedaDestino.Nome}");
+}
+
+var primeiraOrdem = service.ObterTodas().First();
+var busca = service.ObterPorId(primeiraOrdem.OrderId);
+Console.WriteLine($"\nBusca por ID {primeiraOrdem.OrderId}: Valor {busca.ValorOperacao} {busca.MoedaOrigem.Codigo}->{busca.MoedaDestino.Codigo}");
