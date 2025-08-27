302dcb6e35d148414cd6ee3bf1ad4990807bbb5f
victo <victo@SBRISSA>
2025-08-26 22:13:23 -0300

    Commit applications - Services

---
+++ CambioDDD.Application/Services/IOrdemDeCambioService.cs
@@ -0,0 +1,16 @@
+﻿using CambioDDD.Domain.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CambioDDD.Application.Services
+{
+    public interface IOrdemDeCambioService
+    {
+        void Adicionar(decimal valorOperacao, Moeda moedaOrigem, Moeda moedaDestino);
+        OrdemDeCambio? ObterPorId(Guid idOrdem);
+        IEnumerable<OrdemDeCambio> ObterTodas();
+    }
+}
---
+++ CambioDDD.Application/Services/OrdemDeCambioService.cs
@@ -0,0 +1,35 @@
+﻿using CambioDDD.Domain.Orders;
+using CambioDDD.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CambioDDD.Application.Services
+{
+    public class OrdemDeCambioService : IOrdemDeCambioService
+    {
+        private readonly IOrdemDeCambioRepository _repository;
+
+        public OrdemDeCambioService(IOrdemDeCambioRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+        public void Adicionar(decimal valorOperacao, Moeda moedaOrigem, Moeda moedaDestino)
+        {
+            if (valorOperacao <= 0)
+                throw new ArgumentException("Erro - Valor da ordem de câmbio deve ser maior que zero.");
+            OrdemDeCambio ordemDeCambio = new OrdemDeCambio(Guid.NewGuid(), valorOperacao, moedaOrigem, moedaDestino);
+            _repository.Adicionar(ordemDeCambio);
+        }
+        public OrdemDeCambio ObterPorId(Guid idOrdem)
+        {
+            return _repository.ObterPorId(idOrdem);
+        }
+        public IEnumerable<OrdemDeCambio> ObterTodas()
+        {
+           return _repository.ObterTodas();
+        }
+    }
+}
