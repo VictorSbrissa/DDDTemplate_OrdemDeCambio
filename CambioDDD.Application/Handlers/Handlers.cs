eed2bd88d21fef3c502496589757e58e20c3ffa1
victo <victo@SBRISSA>
2025-09-14 17:15:49 -0300

    commit - adicionando novos handlers

--- CambioDDD.Application/Handlers/Handlers.cs
+++ CambioDDD.Application/Handlers/Handlers.cs
@@ -15,4 +15,36 @@ namespace CambioDDD.Application.Handlers
             Console.WriteLine($"[Handler] Ordem criada para cliente {evento.NomeCliente}, valor: {evento.ValorOperacao}");
         }
     }
+
+    public class LogHandler :
+        IHandler<OrdemCriadaEvent>,
+        IHandler<OrdemLiquidadaEvent>,
+        IHandler<OrdemCanceladaEvent>,
+        IHandler<OrdemExpiradaEvent>
+    {
+        public void Handler(OrdemCriadaEvent evento)
+        {
+            Console.WriteLine($"[LOG] Ordem criada: {evento.OrdemId} - Cliente: {evento.NomeCliente}");
+        }
+        public void Handler(OrdemLiquidadaEvent evento)
+        {
+            Console.WriteLine($"[LOG] Ordem criada: {evento.OrdemId} - Valor: {evento.ValorLiquidado}");
+        }
+        public void Handler(OrdemCanceladaEvent evento)
+        {
+            Console.WriteLine($"[LOG] Ordem criada: {evento.OrdemId} - Data: {evento.AcontecidoEm}");
+        }
+        public void Handler(OrdemExpiradaEvent evento)
+        {
+            Console.WriteLine($"[LOG] Ordem criada: {evento.OrdemId} - Data: {evento.AcontecidoEm}");
+        }
+    }
+
+    public class NotificacaoHandler : IHandler<OrdemLiquidadaEvent>
+    {
+        public void Handler(OrdemLiquidadaEvent evento)
+        {
+            Console.WriteLine($"[NOTIFICAÇÃO] Cliente informado da liquidação da ordem {evento.OrdemId}");
+        }
+    }
 }
--- CambioDDD.ConsoleApp/Program.cs
+++ CambioDDD.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CambioDDD.Application.Handlers;
 using CambioDDD.Application.Services;
 using CambioDDD.Domain.Client;
+using CambioDDD.Domain.Enums;
 using CambioDDD.Domain.Events;
 using CambioDDD.Domain.Orders;
 using CambioDDD.Infrastructure.Repositories;
@@ -8,24 +9,43 @@ using static CambioDDD.Domain.Enums.EnumStatus;
 using static CambioDDD.Domain.Orders.OrdemDeCambio;
 
 
-    // 1. Criar dispatcher e registrar handlers
-    var dispatcher = new DomainEventDispatcher();
-    dispatcher.Register(new OrdemCriadaHandler());
+// 1. Criar o dispatcher
+var dispatcher = new DomainEventDispatcher();
 
-    // 2. Criar uma ordem
-    var ordem = new OrdemDeCambio(
-        Guid.NewGuid(),
-        Guid.NewGuid(),
-        1000m,
-        new Moeda("USD", "Dólar Americano"),
-        new Moeda("BRL", "Real Brasileiro"),
-        "João da Silva"
-    );
+// 2. Registrar handlers
+dispatcher.Register(new OrdemCriadaHandler());
+dispatcher.Register<OrdemLiquidadaEvent>(new LogHandler());
+dispatcher.Register(new NotificacaoHandler());
 
-    // 3. Disparar eventos da ordem
-    foreach (var evento in ordem.Eventos)
-    {
-        dispatcher.Dispatch(evento);
-    }
+// 3. Simular disparo de eventos do domínio
+var ordemCriada = new OrdemCriadaEvent(
+    Guid.NewGuid(),
+    Guid.NewGuid(),
+    1000m,
+    EnumStatus.EnumStatusOrdem.Criada,
+    "Cliente Teste",
+    new Moeda("USD", "Dolar"),
+    new Moeda("BRL", "Real")
+);
+dispatcher.Dispatch(ordemCriada);
 
-    Console.ReadLine();
+var ordemLiquidada = new OrdemLiquidadaEvent(
+    ordemCriada.OrdemId,
+    1000m
+);
+dispatcher.Dispatch(ordemLiquidada);
+
+var ordemCancelada = new OrdemCanceladaEvent(
+    ordemCriada.OrdemId,
+    "Cliente desistiu"
+);
+dispatcher.Dispatch(ordemCancelada);
+
+var ordemExpirada = new OrdemExpiradaEvent(
+    ordemCriada.OrdemId
+);
+dispatcher.Dispatch(ordemExpirada);
+
+Console.WriteLine("\n--- FIM DO TESTE ---");
+Console.ReadKey();
+
